4948af9276c93a9b8c38459c3ef23ced962dfd56
alsathik <alsathik@users.noreply.github.com>
2023-08-15 20:20:35 +0530

    Add files via upload

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp
+{
+    partial class wmp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(wmp));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.listView = new System.Windows.Forms.ListView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(156, 495);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.listView);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 59);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(154, 434);
+            this.panel4.TabIndex = 1;
+            // 
+            // listView
+            // 
+            this.listView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(17)))), ((int)(((byte)(121)))));
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView.ForeColor = System.Drawing.Color.White;
+            this.listView.GridLines = true;
+            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(154, 434);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(154)))), ((int)(((byte)(210)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(154, 59);
+            this.panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(10);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(137, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Play List";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.axWindowsMediaPlayer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(156, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(811, 495);
+            this.panel2.TabIndex = 1;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(811, 495);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // wmp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 495);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "wmp";
+            this.Text = "Window Media Player";
+            this.Load += new System.EventHandler(this.wmp_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class wmp : Form
+    {
+        public wmp()
+        {
+            InitializeComponent();
+        }
+        ResourceManager rm = new ResourceManager("WindowsFormsApp.Resources", typeof(Program).Assembly);
+
+        private void wmp_Load(object sender, EventArgs e)
+        {
+            listView.Dock = DockStyle.Fill;
+            listView.View = View.SmallIcon;
+
+            ImageList imageList = new ImageList();
+            imageList.Images.Add(Properties.Resources.favicon);
+            listView.SmallImageList = imageList;
+
+
+            ListViewItem ffx = new ListViewItem("Upbeat", 0);
+            ffx.ImageIndex = 0;
+            listView.Items.Add(ffx);
+
+
+            ListViewItem hud = new ListViewItem("HUD", 1);
+            hud.ImageIndex = 0;
+            listView.Items.Add(hud);
+
+            ListViewItem upbeat = new ListViewItem("FFX", 2);
+            upbeat.ImageIndex = 0;
+            listView.Items.Add(upbeat);
+
+            ListViewItem nat = new ListViewItem("Nat", 2);
+            nat.ImageIndex = 0;
+            listView.Items.Add(nat);
+
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+
+            // Check if an item is selected
+            if (listView.SelectedItems.Count > 0)
+            {
+                int selected = listView.SelectedIndices[0];
+                switch (selected)
+                {
+                    case 0:
+                        filePath = "C:\\Users\\sakub\\Downloads\\upbeat.mp3";
+                        axWindowsMediaPlayer1.URL = filePath;
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                    case 1:
+                        filePath = "C:\\Users\\sakub\\Downloads\\hud.mp4";
+                        axWindowsMediaPlayer1.URL = filePath;
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                    case 2:
+                        filePath = "C:\\Users\\sakub\\Downloads\\ffx.mp4";
+                        axWindowsMediaPlayer1.URL = filePath;
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                    case 3:
+                        filePath = "C:\\Users\\sakub\\Downloads\\nat.mp4";
+                        axWindowsMediaPlayer1.URL = filePath;
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                    default:
+                        filePath = "C:\\Users\\sakub\\Downloads\\upbeat.mp3";
+                        axWindowsMediaPlayer1.URL = filePath;
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                }
+            }
+        }
+    }
+}
